255fc9cfa463d8bde9f888091b4e688a03f4e4c5
Andy Luc <rokaandy@gmail.com>
2017-06-16 03:20:15 -0700

    Modify HTML and JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  
-}
+  $("#blanks form").submit(function(event) {
+    var temperature = $("input#temperature").val();
+    var nature = $("input#nature").val();
+    var surface = $("input#surface").val();
+    var color = $("input#color").val();
+    var hotel = $("input#hotel").val();
+  });
+});
