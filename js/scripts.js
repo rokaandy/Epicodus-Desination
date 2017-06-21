c6be69a7500a9deead08666b4f7865b11ea5e8b7
Andy Luc <rokaandy@gmail.com>
2017-06-20 23:50:18 -0700

    Modify HTML, CSS and JS files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  $('#destination1').hide();
+  $('#destination2').hide();
+  $('#destination3').hide();
   $("#blanks form").submit(function(event) {
     var temperatureInput = $("input#temperature").val();
     var natureInput = $("input#nature").val();
@@ -6,8 +9,24 @@ $(document).ready(function() {
     var colorInput = $("input#color").val();
     var hotelInput = $("input#hotel").val();
 
-    //$('')
-
+    if (temperatureInput === "A" || temperatureInput === "a") {
+      $('#destination1').show();
+    }
+    if (temperatureInput === "B" || temperatureInput === "b") {
+      $('#destination2').show();
+    }
+    if (natureInput === "B" || temperatureInput === "b") {
+      $('#destination2').show();
+    }
+    if (colorInput === "A" || colorInput === "a") {
+      $('#destination3').show();
+    }
+    if (colorInput === "B" || colorInput === "b") {
+      $('#destination1').show();
+    }
+    if (surfaceInput === "A" || surfaceInput === "a") {
+      $('#destination3').show();
+    }
     event.preventDefault();
   });
 });
